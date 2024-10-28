7192a7827744c4f6b9e033017d352c4812e7b7f4
mohan <mohan.talachutla@gmail.com>
2024-10-28 14:44:21 +0530

    refactor, fix git cache

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 const isProd = process.env.NODE_ENV === 'production'
+
 export default {
   input: 'src/index.js',
   output: [
@@ -13,32 +14,11 @@ export default {
       file: 'lib/index.cjs',
       format: 'cjs',
     },
-    {
-      name: 'utils',
-      file: 'lib/index.umd.js',
-      format: 'umd',
-    },
-    {
-      file: 'lib/index.iife.js',
-      format: 'iife',
-    },
-    {
-      file: 'lib/index.system.js',
-      format: 'iife',
-    },
-    {
-      file: 'lib/index.amd.js',
-      format: 'amd',
-    },
-    // {
-    //   file: 'lib/index.min.js',
-    //   format: 'esm',
-    //   plugins: [terser({ module: true })],
-    // },
   ],
   plugins: [
     resolve(),
     commonjs(),
     isProd && import('@rollup/plugin-terser').then((terser) => terser.default),
+    // configure plugins for eslint, prettier, and husky
   ],
 }
